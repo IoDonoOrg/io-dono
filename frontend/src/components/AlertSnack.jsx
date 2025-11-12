b457a259bee76e0fe2d5a767237180b167c8b597
Mionuel <lifevlad2015@gmail.com>
2025-11-12 11:52:36 +0100

    nuovo componente per creare degli alert

---
+++ frontend/src/components/AlertSnack.jsx
@@ -0,0 +1,33 @@
+import { Alert, Snackbar } from "@mui/material";
+
+// attenzione: il componente dipende fortissimo dal genitore in cui verrà usato
+// --> non ha stato interno, dipende completamente degli stati del genitore
+function AlertSnack({ children, severity, open, onClose }) {
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    // chiama onClose solo è stato fornito nei proprs
+    onClose?.();
+  };
+
+  return (
+    <Snackbar
+      open={open}
+      autoHideDuration={10000}
+      anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      onClose={handleClose}
+    >
+      <Alert
+        severity={severity}
+        onClose={handleClose}
+        variant="filled"
+        sx={{ width: "100%" }}
+      >
+        {children}
+      </Alert>
+    </Snackbar>
+  );
+}
+
+export default AlertSnack;
