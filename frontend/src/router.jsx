98792095a79e4745415e53392aecb4e6ba386f71
Mionuel <lifevlad2015@gmail.com>
2025-11-14 19:26:51 +0100

    cancellato vecchio componenete App (non è più utilizzato)

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import App from "./pages/App/App";
 import Example from "./pages/Example/Example";
 import Login from "./pages/Login/Login";
 import NotFound from "./pages/NotFound/NotFound";
@@ -8,10 +7,10 @@ import NotFound from "./pages/NotFound/NotFound";
 // Definisce il router principale dell'app
 // Ogni oggetto rappresenta una rotta con il suo percorso (path) e il componente da mostrare (element)
 const router = createBrowserRouter([
-  { path: "/", element: <App /> },
+  { path: "/", element: <Example /> },
   { path: "/example", element: <Example /> },
   { path: "/login", element: <Login /> },
-  { path: "/registration", element: <Login /> },
+  // { path: "/registration", element: <Login /> },
   { path: "*", element: <NotFound /> },
 ]);
 
