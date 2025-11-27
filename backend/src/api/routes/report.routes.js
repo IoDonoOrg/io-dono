23e964f424ab78488e1d13259ccb04b714a15b6c
Mionuel <lifevlad2015@gmail.com>
2025-11-27 18:30:26 +0100

    commentato un pezzo di codice che crashava il programma

--- backend/src/api/routes/report.routes.js
+++ backend/src/api/routes/report.routes.js
@@ -1,58 +1,58 @@
 const express = require('express');
 const reportController = require('../controllers/report.controller');
-const { isAuth, isAdmin } = require('../../middleware/auth.middleware.js'); 
+const { isAuth, isAdmin } = require('../../middleware/auth.middleware.js');
 
 const router = express.Router();
 
 // POST /api/reports 
 // Crea una nuova segnalazione
 router.post(
-    '/', 
-    isAuth, 
-    reportController.createReport
+  '/',
+  isAuth,
+  reportController.createReport
 );
 
 // GET /api/reports/me/open
 //  Vedi le tue segnalazioni inviate
 router.get(
-    '/me/open', 
-    isAuth, 
-    reportController.getMyOpenReports
+  '/me/open',
+  isAuth,
+  reportController.getMyOpenReports
 );
 
 // GET /api/reports/me/closed
 //  Vedi le tue segnalazioni inviate
 router.get(
-    '/me/closed', 
-    isAuth, 
-    reportController.getMyClosedReports
+  '/me/closed',
+  isAuth,
+  reportController.getMyClosedReports
 );
 
 // GET /api/reports/me/admin/open
 // Vedi tutte le segnalazioni (Dashboard Admin)
-router.get(
-    '/admin/open', 
-    isAuth, 
-    isAdmin, 
-    reportController.getAllClosedOpenReports
-);
+// router.get(
+//     '/admin/open', 
+//     isAuth, 
+//     isAdmin, 
+//     reportController.getAllClosedOpenReports
+// );
 
 // GET /api/reports/me/admin/closed
 // Vedi tutte le segnalazioni (Dashboard Admin)
 router.get(
-    '/admin/closed', 
-    isAuth, 
-    isAdmin, 
-    reportController.getAllOpenReports
+  '/admin/closed',
+  isAuth,
+  isAdmin,
+  reportController.getAllOpenReports
 );
 
 // PATCH /api/reports/:id/status
 // Cambia stato 
 router.patch(
-    '/:id/status', 
-    isAuth, 
-    isAdmin, 
-    reportController.updateReportStatus
+  '/:id/status',
+  isAuth,
+  isAdmin,
+  reportController.updateReportStatus
 );
 
 module.exports = router;
