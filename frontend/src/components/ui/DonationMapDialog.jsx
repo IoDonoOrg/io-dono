21526fc443c8c60d0b61005883d300e7d48abf15
Mionuel <lifevlad2015@gmail.com>
2026-01-29 17:32:07 +0100

    aggiunto il css per la mappa leaflet

--- frontend/src/components/ui/DonationMapDialog.jsx
+++ frontend/src/components/ui/DonationMapDialog.jsx
@@ -12,6 +12,8 @@ import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { useDonation } from "src/hooks/useDonation";
 import { formatDate } from "src/utils/format";
 
+import "leaflet/dist/leaflet.css";
+
 // TODO: idealmente dovrebbero essere le coordinate di indirizzo dell'utente
 // Per ora sono coordinate di Trento
 const DEFAULT_START_COORDS = [46.06787, 11.12108];
@@ -52,41 +54,44 @@ export default function DonationMapDialog({ open, onClose }) {
 
       <DialogContent dividers sx={{ p: 0 }}>
         <Box sx={{ height: "100%", width: "100%" }}>
-          <MapContainer
-            center={DEFAULT_START_COORDS}
-            zoom={13}
-            style={{ height: "100%", width: "100%" }}
-          >
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            {allDonations?.map((donation) => (
-              <Marker
-                key={donation._id}
-                // GeoJSON è [Lng, Lat], Leaflet aspetta [Lat, Lng] => scambio
-                position={[
-                  donation.pickupLocation.geo.coordinates[1],
-                  donation.pickupLocation.geo.coordinates[0],
-                ]}
-              >
-                <Popup>
-                  <div>
-                    <strong>ID:</strong> {donation._id.substring(0, 10)} <br />
-                    <strong>Data ritiro:</strong>{" "}
-                    {formatDate(donation.pickupTime)} <br />
-                    <strong>Indirizzo:</strong> <br />
-                    {donation.pickupLocation.address}
-                    <br />
-                    <strong>Contenuti:</strong>
-                    <br />{" "}
-                    {donation.items.map((i, index) => (
-                      <div key={index}>
-                        {i.name} ({i.quantity})
-                      </div>
-                    ))}
-                  </div>
-                </Popup>
-              </Marker>
-            ))}
-          </MapContainer>
+          {open && (
+            <MapContainer
+              center={DEFAULT_START_COORDS}
+              zoom={13}
+              style={{ height: "100%", width: "100%" }}
+            >
+              <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+              {allDonations?.map((donation) => (
+                <Marker
+                  key={donation._id}
+                  // GeoJSON è [Lng, Lat], Leaflet aspetta [Lat, Lng] => scambio
+                  position={[
+                    donation.pickupLocation.geo.coordinates[1],
+                    donation.pickupLocation.geo.coordinates[0],
+                  ]}
+                >
+                  <Popup>
+                    <div>
+                      <strong>ID:</strong> {donation._id.substring(0, 10)}{" "}
+                      <br />
+                      <strong>Data ritiro:</strong>{" "}
+                      {formatDate(donation.pickupTime)} <br />
+                      <strong>Indirizzo:</strong> <br />
+                      {donation.pickupLocation.address}
+                      <br />
+                      <strong>Contenuti:</strong>
+                      <br />{" "}
+                      {donation.items.map((i, index) => (
+                        <div key={index}>
+                          {i.name} ({i.quantity})
+                        </div>
+                      ))}
+                    </div>
+                  </Popup>
+                </Marker>
+              ))}
+            </MapContainer>
+          )}
         </Box>
       </DialogContent>
     </Dialog>
