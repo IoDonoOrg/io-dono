b2370579b7c50e478734fd919a6f819e6397b108
Mionuel <lifevlad2015@gmail.com>
2026-05-10 16:27:50 +0200

    feat: dashboard admin - inizio

--- frontend/src/pages/Dashboard/AdminDashboard.jsx
+++ frontend/src/pages/Dashboard/AdminDashboard.jsx
@@ -1,9 +1,61 @@
+import HeaderBar from "src/components/ui/HeaderBar";
+import { Link as RouterLink } from "react-router-dom";
+import { Box, Container, Paper } from "@mui/material";
+
+import { useState } from "react";
+import ActiveDonationsTile from "src/components/ui/ActiveDonationsTile";
+import { DonationProvider } from "src/context/DonationProvider";
+import DonationHistory from "src/components/ui/DonationHistory";
+import TileClickable from "src/components/ui/TileClickable";
+import DonationMapDialog from "src/components/ui/DonationMapDialog";
+import { DIALOGS } from "src/utils/constants";
+import AssociationStatistics from "src/components/ui/AssociationStatistics";
+
 function AdminDashboard() {
+  const [openDialog, setOpenDialog] = useState(null);
+
   return (
     <>
-      <div>
-        <p>Admin dashboard</p>
-      </div>
+      {/* 
+        DonationProvider è il contesto React che ci permette di aggiornare 
+        l'elenco delle donazione appena ne viene creata / cancellata / modificata una 
+      */}
+      <DonationProvider>
+        <HeaderBar />
+        <Container className="flex flex-col items-center justify-between mx-15 my-5 p-2">
+          <Paper
+            className="flex flex-col items-center space-y-4 p-5 w-3/4"
+            sx={{
+              borderRadius: 2,
+            }}
+          >
+            <ActiveDonationsTile displayNumber={4} />
+            <DonationHistory
+              open={openDialog === DIALOGS.HISTORY}
+              onClose={() => setOpenDialog(null)}
+            />
+            <DonationMapDialog
+              open={openDialog === DIALOGS.MAP}
+              onClose={() => setOpenDialog(null)}
+            />
+            <AssociationStatistics
+              open={openDialog === DIALOGS.STATISTICS}
+              onClose={() => setOpenDialog(null)}
+            />
+            <Box className="flex flex-row gap-10 my-5">
+              <TileClickable onClick={() => setOpenDialog(DIALOGS.HISTORY)}>
+                Donazioni
+              </TileClickable>
+              <TileClickable onClick={() => setOpenDialog(DIALOGS.MAP)}>
+                Mappa
+              </TileClickable>
+              <TileClickable onClick={() => setOpenDialog(DIALOGS.STATISTICS)}>
+                Statistiche
+              </TileClickable>
+            </Box>
+          </Paper>
+        </Container>
+      </DonationProvider>
     </>
   );
 }
